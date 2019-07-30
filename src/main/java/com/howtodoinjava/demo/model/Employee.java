01b68aa5824e6770bff7de90ba45a0a1c48a8ab9
hi2sanjay <hi2sanjay@gmail.com>
2019-07-30 03:06:14 +0100

    updated for build

--- src/main/java/com/howtodoinjava/demo/model/Employee.java
+++ src/main/java/com/howtodoinjava/demo/model/Employee.java
@@ -45,6 +45,6 @@ public class Employee {
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", firstName=" + firstName
-				+ ", lastName=" + lastName + ", email=" + email + "]";
+				+ ", lastName=" + lastName + ", email=" + email + " mail for the changes]";
 	}
 }
